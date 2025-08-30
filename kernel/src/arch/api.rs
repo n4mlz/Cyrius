21a0d1351414e7ffcc4821db5b3a027e784f30cf
n4mlz <n4mlz.dev@gmail.com>
2025-08-30 23:36:22 +0900

    refactor: remove unused ArchBootInfo trait and related imports

--- kernel/src/arch/api.rs
+++ kernel/src/arch/api.rs
@@ -1,14 +1,12 @@
-use crate::mem::bootinfo::KernelSegments;
-
 pub trait ArchPlatform {
     fn name() -> &'static str;
+
+    /// platform-specific initialization
+    ///
+    /// After executing this function, we expect the BSS to be initialized and the stack pointer to be properly set.
     fn init();
 }
 
 pub trait ArchDevice {
     fn console() -> &'static dyn crate::device::char::uart::Uart<Error = ()>;
-}
-
-pub trait ArchBootInfo {
-    fn kernel_segments() -> &'static KernelSegments;
 }
