e878e2d3544dbf7710df9fbf0bbb5452d0e3cc8f
n4mlz <n4mlz.dev@gmail.com>
2025-12-08 17:27:35 +0900

    feat: Compress OCI Bundle to tar

--- make_bundle.sh
+++ make_bundle.sh
@@ -4,12 +4,16 @@ cd "$(dirname "$0")"
 
 IMAGE="docker://busybox:musl"
 OCI_DIR="busybox-oci"
-BUNDLE_DIR="mnt/bundle"
+BUNDLE_DIR="bundle"
+BUNDLE_TAR="mnt/bundle.tar"
 
-rm -rf "$OCI_DIR" "$BUNDLE_DIR"
+rm -rf "$OCI_DIR" "$BUNDLE_DIR" "$BUNDLE_TAR"
+mkdir -p "$(dirname "$BUNDLE_DIR")"
 
 skopeo copy "$IMAGE" "oci:$OCI_DIR:latest"
 umoci unpack --rootless --image "$OCI_DIR:latest" "$BUNDLE_DIR"
 
-rm -rf "$OCI_DIR" "$BUNDLE_DIR"/umoci.json "$BUNDLE_DIR"/sha256_*.mtree
+rm -f "$BUNDLE_DIR"/umoci.json "$BUNDLE_DIR"/sha256_*.mtree
+tar --format=posix --numeric-owner -C "$BUNDLE_DIR" -cf "$BUNDLE_TAR" .
 
+rm -rf "$OCI_DIR" "$BUNDLE_DIR"
